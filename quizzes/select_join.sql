482d8998357df9238d7ab6cad2845c674c55fd43
ZawadiSumba66 <rosesumba98@gmail.com>
2021-02-03 17:33:18 +0300

    Add more join

--- quizzes/select_join.sql
+++ quizzes/select_join.sql
@@ -36,6 +36,11 @@ SELECT stadium, COUNT(player) as goals
 FROM game JOIN goal ON id=matchid
 GROUP BY stadium
 
+SELECT matchid,mdate, COUNT(teamid)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'POL' OR team2 = 'POL')
+GROUP by matchid,mdate
+
 SELECT matchid, mdate ,COUNT(player) as goals
 FROM game JOIN goal ON matchid=id
 WHERE teamid='GER'
